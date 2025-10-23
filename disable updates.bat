af4ab6aad5afe0cb19b5fb0624155bf149498801
tsgrgo <37509858+tsgrgo@users.noreply.github.com>
2025-10-23 20:54:45 +0200

    Fix issue with long paths

--- disable updates.bat
+++ disable updates.bat
@@ -4,10 +4,15 @@
 :: Get admin and system privileges
 if not "%1"=="admin" (powershell start -verb runas '%0' admin & exit /b)
 if not "%2"=="system" (
-	mode con: cols=120 && powershell -NoProfile -Command "$host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.Size(120,30)"
-	schtasks /Create /tn "%~nx0" /tr "'%~f0' admin system > '%temp%\%~nx0' && timeout /t 1 && del '%temp%\%~nx0'" /sc ONCE /st 00:00 /rl HIGHEST /f /ru SYSTEM
-	schtasks /Run /tn "%~nx0" && schtasks /Delete /tn "%~nx0" /f
-	powershell -NoProfile -Command "Get-Content '%temp%\%~nx0'; Get-Content '%temp%\%~nx0' -Wait -Tail 0 -ErrorAction SilentlyContinue"
+	echo "%~f0" admin system ^> "%temp%\%~nx0.log" ^& timeout /t 1 ^& del "%temp%\%~nx0.log" "%temp%\%~nx0" > "%temp%\%~nx0"
+
+  	schtasks /Create /tn "%~nx0" /tr "'%temp%\%~nx0'" /sc ONCE /st 00:00 /rl HIGHEST /f /ru SYSTEM
+	schtasks /Run /tn "%~nx0"
+	schtasks /Delete /tn "%~nx0" /f
+
+	start /b powershell -NoProfile -command "&{$w=(get-host).ui.rawui;$w.buffersize=@{width=120;height=999};$w.windowsize=@{width=120;height=30};}"
+	powershell -NoProfile -Command "Get-Content '%temp%\%~nx0.log' -Wait -Encoding oem -ErrorAction SilentlyContinue"
+
 	pause && exit /b
 )
 
